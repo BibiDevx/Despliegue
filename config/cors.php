68d7a93b3b8f006cf7c6f71559e760885b74c475
Brian Beltran <babeltranp@gmail.com>
2025-12-16 09:25:54 -0500

    change cors jwt

--- config/cors.php
+++ config/cors.php
@@ -9,16 +9,17 @@ return [
     'allowed_origins' => [
         'https://despliguefront-production.up.railway.app',
         'https://despliguefront-production-56be.up.railway.app',
+        'http://localhost:3000'
     ],
 
     'allowed_origins_patterns' => [],
 
     'allowed_headers' => ['*'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Authorization'],
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
